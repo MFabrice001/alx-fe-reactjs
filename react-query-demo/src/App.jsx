87db5ab245e8865ae83216697079a75646789140
MFabrice001 <m.fabrice001@gmail.com>
2024-08-28 20:17:49 +0200

    updated

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,38 +1,14 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
 
-const fetchPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-};
+const queryClient = new QueryClient();
 
-const PostsComponent = () => {
-  const { data, error, isLoading, isError } = useQuery('posts', fetchPosts);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error: {error.message}</div>;
-  }
-
+function App() {
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {data.map(post => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
   );
-};
+}
 
-export default PostsComponent;
+export default App;

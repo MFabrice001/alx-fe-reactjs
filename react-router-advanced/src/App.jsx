7500821e081dd3f22f59c15542188651233b3449
MFabrice001 <m.fabrice001@gmail.com>
2024-08-29 21:15:52 +0200

    updated

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

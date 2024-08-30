8ee97599a1ad28fe7c061ce43474ecbb5b61f838
MFabrice001 <m.fabrice001@gmail.com>
2024-08-30 20:58:41 +0200

    updated

---
+++ react-router-advanced/src/components/useAuth.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  return isAuthenticated;
+};
+
+export default useAuth;

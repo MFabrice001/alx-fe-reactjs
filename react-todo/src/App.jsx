92ffae84ee00a780298f25f8d5976431759f2473
MFabrice001 <m.fabrice001@gmail.com>
2024-08-31 23:08:25 +0200

    updated

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import TodoList from './TodoList';
+import TodoList from './components/TodoList';
 
 function App() {
   const [count, setCount] = useState(0)
--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from '../TodoList';
+import TodoList from '../components/TodoList';
 
 test('renders initial todos', () => {
   render(<TodoList />);

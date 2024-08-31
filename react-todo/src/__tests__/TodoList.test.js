5e476143a2819e3ca03d2592f08e433102fef760
MFabrice001 <m.fabrice001@gmail.com>
2024-08-31 13:54:33 +0200

    updated

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TodoList from '../TodoList';
+
+test('renders initial todos', () => {
+  render(<TodoList />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  expect(screen.getByText('Build a Todo App')).toBeInTheDocument();
+});
+
+test('can add a new todo', () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText('Add a new todo'), { target: { value: 'Write tests' } });
+  fireEvent.click(screen.getByText('Add Todo'));
+  expect(screen.getByText('Write tests')).toBeInTheDocument();
+});
+
+test('can toggle a todo', () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getByText('Learn React'));
+  expect(screen.getByText('Learn React')).toHaveStyle('text-decoration: line-through');
+});
+
+test('can delete a todo', () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getByText('Delete', { selector: 'button' }));
+  expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+});

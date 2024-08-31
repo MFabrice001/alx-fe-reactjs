f9c78206550d090decb1d186fa25d565fbd28a56
MFabrice001 <m.fabrice001@gmail.com>
2024-08-31 13:09:42 +0200

    updated

--- react-todo/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest'
-    },
-    moduleFileExtensions: ['js', 'jsx']
-  };
-  

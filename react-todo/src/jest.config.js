58eddba59bb7108a71884db343aa25143f4c31ff
MFabrice001 <m.fabrice001@gmail.com>
2024-09-04 22:32:38 +0200

    updated

---
+++ react-todo/src/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    transform: {
+      "^.+\\.jsx?$": "babel-jest"
+    },
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["js", "jsx"],
+    setupFilesAfterEnv: ["<rootDir>/setupTests.js"]
+  };
+  
---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,13 @@
+/module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to all of your template files
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+

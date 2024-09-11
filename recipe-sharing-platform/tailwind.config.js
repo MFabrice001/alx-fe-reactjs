b2105d2a822da39289e4b747484f434c79180fae
MFabrice001 <m.fabrice001@gmail.com>
2024-09-11 07:58:43 +0200

    updated

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'media',
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}

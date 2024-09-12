755bf2b77a44ff959c48ada36f65248570ecaba7
MFabrice001 <m.fabrice001@gmail.com>
2024-09-12 07:42:09 +0200

    updated

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',  // Make sure these paths match your project's structure
     './public/index.html',
   ],
   theme: {

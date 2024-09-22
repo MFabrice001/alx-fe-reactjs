f7655f406bbcdc6b2eca9153317551fdd26c81d3
MFabrice001 <m.fabrice001@gmail.com>
2024-09-22 16:59:16 +0200

    updated

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,19 @@
+import axios from 'axios';
+
+export async function fetchUserData(username) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching user data: ' + error.message);
+  }
+}
+
 export async function fetchUsers(query) {
-  const response = await fetch(`https://api.github.com/search/users?${query}`);
-  const data = await response.json();
-  return data.items;
+  try {
+    const response = await axios.get(`https://api.github.com/search/users?${query}`);
+    return response.data.items;
+  } catch (error) {
+    throw new Error('Error fetching users: ' + error.message);
+  }
 }
